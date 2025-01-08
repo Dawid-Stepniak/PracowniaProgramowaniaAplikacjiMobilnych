a577bdb81699a32201b0051833d22a95320852ea
student <dawid.stepniak2006@gmail.com>
2025-01-08 08:40:00 +0100

    Do dokończenia

---
+++ 2025_01_8_wizyta_u_weterynarza/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "2025_01_8_wizyta_u_weterynarza"
+include(":app")
